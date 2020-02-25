e4909b8d56f9d805f07cdee1aedca87b67b9f79c
Romain WALLON <wallon@cril.fr>
2020-02-25 18:24:35 +0100

    :white_check_mark: Prepares tests for the pseudo-Boolean solving part.

---
+++ src/test/java/fr/univartois/cril/pbd4/pbc/TestDualHypergraphPartitioning.java
@@ -0,0 +1,51 @@
+/**
+ * PBD4, a pseudo-Boolean based implementation of the D4 compiler.
+ * Copyright (c) 2020 - Univ Artois & CNRS.
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see {@link http://www.gnu.org/licenses}.
+ */
+
+package fr.univartois.cril.pbd4.pbc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * The TestDualHypergraphPartitioning
+ *
+ * @author Romain WALLON
+ *
+ * @version 0.1.0
+ */
+class TestDualHypergraphPartitioning {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
+
